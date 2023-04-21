9f6541735b311e6652bc7a9f92d3835bd1600f04
Arkadii Chumachenko <arkadii.chumachenko@getvisibility.com>
2023-04-21 13:10:14 +0100

    feat: rollback

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary
-	version string = "0.1.2"
+	version string = "dev"
 
 	// goreleaser can also pass the specific commit if you want
 	// commit  string = ""
